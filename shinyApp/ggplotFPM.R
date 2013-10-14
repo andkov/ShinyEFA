31a0f07b1eb14ae9fb48c36a650201e1a3ef2699
Will Beasley <wibeasley@hotmail.com>
2013-10-13 20:26:11 -0500

    prototype factor loading graph
    while on the phone

--- shinyApp/ggplotFPM.R
+++ shinyApp/ggplotFPM.R
@@ -23,36 +23,56 @@ R<-Harman.Holzinger
   FPM <- cbind(FPM, matrix(numeric(0), p, p-k)) # appends empty columns to have p columns
   colnames(FPM) <- paste0("F", 1:p) # renames for better presentation in tables and graphs
 
-  # output
+roundingDigits <- 2 #Let the user define?
+stripSize <- 12  #Let the user define?
+title <- NULL #"Basic Title" #Read from the dataset name or rotation?
+# output
   # Data prep for ggplot
-  dsFORp <- reshape2::melt(FPM, id.vars=rownames(FPM))  ## id.vars declares MEASURED variables (as opposed to RESPONSE variable)
-  dsFORp <- plyr::rename(dsFORp, replace=c(Var1="Variable", Var2="Factor", value="Loading"))
-  dsFORp$positive <- dsFORp$Loading >= 0 # is factor loading positive? color coded in ggplot
-  dsFORp$Loading <- abs(as.numeric(dsFORp$Loading)) # Long form
-  # The colors for negative and positve values of factor loadings for ggplot
-  colors <- c("darksalmon" ,"lightskyblue")
-  title <- "Basic Title"
+dsFORp <- reshape2::melt(FPM, id.vars=rownames(FPM))  ## id.vars declares MEASURED variables (as opposed to RESPONSE variable)
+dsFORp <- plyr::rename(dsFORp, replace=c(Var1="Variable", Var2="Factor", value="Loading"))
+dsFORp$Positive <- ifelse(dsFORp$Loading >= 0, "Positive", "Negative") #Or see Recipe 10.8
+dsFORp$LoadingAbs <- abs(dsFORp$Loading) # Long form
+dsFORp$LoadingPretty <- round(abs(dsFORp$Loading), roundingDigits) # Long form
+dsFORp$VariablePretty <- gsub(pattern="_", replacement="\n", x=dsFORp$Variable)
+# colors <- c("FALSE"="darksalmon" ,"TRUE"="lightskyblue") # The colors for negative and positve values of factor loadings for ggplot
+colorsFill <- c("Positive"="#A6CEE3" ,"Negative"="#B2DF8A") # The colors for negative and positve values of factor loadings for ggplot
+colorsColor <- c("Positive"="#00CEE3" ,"Negative"="#00DF8A") # The colors for negative and positve values of factor loadings for ggplot
+
+
   # Graph definition
-  pp <- ggplot(dsFORp, aes(x=Factor, y=Loading, fill=positive)) +
-    ggtitle(title) + 
-    geom_bar(stat="identity") +
-    geom_text(aes(label=Loading), vjust=1.5, colour="black")+
-    scale_fill_manual(values=colors) +
-    scale_y_continuous(limits=c(0,1)) +
-    facet_grid(Variable ~ .)+
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.background =element_rect(fill="white"),
-          axis.text.x=element_text(angle=0, hjust=.5))
-  print(pp)
-
-
-
-
-# Below the top
-ggplot(cabbage_exp, aes(x=interaction(Date, Cultivar), y=Weight)) +
+pp <- ggplot(dsFORp, aes(x=Factor, y=LoadingAbs, fill=Positive, color=Positive, label=LoadingPretty)) +
   geom_bar(stat="identity") +
-  geom_text(aes(label=Weight), vjust=1.5, colour="white")
+  geom_text(y=0, vjust=-.1) +
+  scale_color_manual(values=colorsColor, guide="none") +
+  scale_fill_manual(values=colorsFill) +
+#   scale_fill_discrete(h=c(0,360)+15, c=100, l=65, h.start=0, direction=1, na.value="grey50") + #http://docs.ggplot2.org/0.9.3/scale_hue.html
+  scale_y_continuous(limits=c(0,1), breaks=c(.5, 1), expand=c(0,0)) +
+  facet_grid(VariablePretty ~ .) +
+  labs(title=title, x="FFF", y="Loadings (Absolute)", fill=NULL) + 
+  theme_bw() +
+  theme(panel.grid.minor=element_blank()) + 
+#   theme(axis.label=element_text(color="gray30")) +  
+  theme(axis.text.y=element_text(color="gray50")) +   
+  theme(strip.text.y=element_text(angle=0, size=stripSize))
+
+{
+  if( k < p ) {
+    pp <- pp + theme(legend.position=c(1, 0), legend.justification=c(1, 0)) 
+    pp <- pp + theme(legend.background=element_rect(fill="gray70"))
+  }
+  else {
+    pp <- pp + theme(legend.position="left")
+  }
+}
+print(pp)
+
+
+
+
+# # Below the top
+# ggplot(cabbage_exp, aes(x=interaction(Date, Cultivar), y=Weight)) +
+#   geom_bar(stat="identity") +
+#   geom_text(aes(label=Weight), vjust=1.5, colour="white")
 
 
 
