99592b1ce6b2de92df7261169a483a45e95e1883
wibeasley <wibeasley@hotmail.com>
2013-12-05 12:04:22 -0600

    Specifying a fixed port.  I'm testing what will happen with the Shiny Apps I'll deploy to the OUHSC server

--- shinyApp/run.R
+++ shinyApp/run.R
@@ -5,4 +5,4 @@ library(shiny)
 # setwd("D:/Users/Will/Documents/GitHub/ShinyEFA") #Will's Development Box
 # setwd("~/ShinyEFA") #Will's Experimental Shiny server at home: http://lucky1304:8100/
 server <- (Sys.info()["nodename"] == "Lucky1304")
-shiny::runApp('shinyApp', launch.browser=!server)
+shiny::runApp('shinyApp', launch.browser=!server, port=8100)
