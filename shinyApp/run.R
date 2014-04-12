0dc492ec44a05176d37d163133f1b7090cac8247
Will Beasley <wibeasley@insightresults.com>
2014-04-12 17:05:13 -0500

    open different port for BBMC server

--- shinyApp/run.R
+++ shinyApp/run.R
@@ -4,5 +4,7 @@ library(shiny)
 # setwd("C:/Users/kovalav/Documents/GitHub/ShinyEFA")
 # setwd("D:/Users/Will/Documents/GitHub/ShinyEFA") #Will's Development Box
 # setwd("~/ShinyEFA") #Will's Experimental Shiny server at home: http://lucky1304:8100/
-server <- (Sys.info()["nodename"] == "Lucky1304")
-shiny::runApp('shinyApp', launch.browser=!server, port=8100)
+server <- (Sys.info()["nodename"] %in% c("Lucky1304", "bbmc-shiny-public"))
+bbmcServer <- (Sys.info()["nodename"]=="bbmc-shiny-public")
+port <- ifelse( bbmcServer, 3838, 8100)
+shiny::runApp('shinyApp', launch.browser=!server, port=3838)
